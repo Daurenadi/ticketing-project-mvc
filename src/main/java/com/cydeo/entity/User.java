056c211b3e8d146959aa03c0100db34f43b1a57b
Dauren <daurenadi@gmail.com>
2024-05-18 16:18:06 -0400

    User and Role entities created

--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -1,22 +1,21 @@
 package com.cydeo.entity;
 
-import com.cydeo.dto.RoleDTO;
+
 import com.cydeo.enums.Gender;
-import com.cydeo.model.Role;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.*;
-import java.util.EnumSet;
+
 
 @Entity
 @Table(name = "users")
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class User extends BaseEntity{
@@ -28,6 +27,7 @@ public class User extends BaseEntity{
     private String password;
     private boolean enabled;
     private String phone;
+    @ManyToOne(fetch = FetchType.LAZY)
     private Role role;
     private Gender gender;
 
